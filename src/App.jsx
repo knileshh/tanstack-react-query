70377c0575829d775f0ee289aad1de97b1a0d0bd
knilesh9 <neutron_stays@protonmail.com>
2024-05-09 22:34:42 +0530

    Added query-dev tools with library

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,29 @@ function App(){
     queryFn: () => wait(1000).then(() => [...POSTS]),
   })
 
+  const newPostMutation = useMutation({
+    mutationFn: title => {
+      return wait(1000).then(() => {
+        POSTS.push({id: crypto.randomUUID(), title})
+      })
+    },
+  })
+
   if(postsQuery.isLoading) {
     return <h1>Loading....</h1>
   }
 
-  return <h1>Tanstack Query </h1>
+  if(postsQuery.isError){
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>
+  }
+
+  return (
+    <div>
+      {postsQuery.data.map(post => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+    </div>
+  )
 
 }
 
